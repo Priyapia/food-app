4649ba80577148bd5bdccbd22aa1ded5e29df1e2
Anush Kumar <anush.kumar@fortheye.co>
2024-02-18 19:41:06 +0530

    Fixes

--- src/foodApp/MealItem.jsx
+++ src/foodApp/MealItem.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import meal from "./available-meals.json";
-// import image from "../images/mac-and-cheese.jpg";
-const MealItem = () => {
+import { useEffect, useState } from "react";
+const MealItem = (props) => {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    setLoadedMeals(props.data);
+  }, [props.data]);
+
   return (
     <>
-      <li className="meal-item">
-        <article>
-          {/* <img src={image} alt="imggg" /> */}
-          {meal.map((meal) => (
-            <>
-              <img src={meal.image} alt={meal.name} />
-              <div>
-                <h3>{meal.name}</h3>
-                <p className="meal-item-price">{meal.price}</p>
-                <p className="meal-item-description">{meal.description}</p>
-              </div>
-              <p className="meal-item-action">
-                <button> Add To Cart </button>
-              </p>
-            </>
-          ))}
-        </article>
-      </li>
+      {loadedMeals.map((meal) => {
+        return (
+          <li className="meal-item" key={meal.id}>
+          <img src={meal.image} alt={meal.name} />
+          <div>
+            <h3>{meal.name}</h3>
+            <p className="meal-item-price">{meal.price}</p>
+            <p className="meal-item-description">{meal.description}</p>
+          </div>
+          <p className="meal-item-action">
+            <button> Add To Cart </button>
+          </p>
+        </li>
+        )
+      })}
     </>
   );
 };
--- src/foodApp/Meals.jsx
+++ src/foodApp/Meals.jsx
@@ -1,32 +1,14 @@
-import React, { useEffect, useState } from "react";
 import meal from "./available-meals.json";
 import MealItem from "./MealItem";
+
 function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState();
-  let array = [];
-  useEffect(() => {
-    for (let i = 0; i < meal.length; i++) {
-      // console.log(meal[i].name);
-      array.push(meal[i].name);
-
-      const aa = array.map((meal, i) => (
-        <p
-          key={i}
-          style={{
-            margin: "10px",
-          }}
-        >
-          {meal}
-          <MealItem />
-        </p>
-      ));
-      setLoadedMeals(aa);
-    }
-  }, []);
-
   return (
     <>
-      <ul id="meals">{loadedMeals}</ul>
+      <div>
+        <ul>
+          <MealItem data={meal} />
+        </ul>
+      </div>
     </>
   );
 }
