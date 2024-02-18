738753e07f366aa432cbadefc3c371d2336c107a
Anush Kumar <anush.kumar@fortheye.co>
2024-02-18 19:49:32 +0530

    Fixes

--- src/foodApp/MealItem.jsx
+++ src/foodApp/MealItem.jsx
@@ -11,17 +11,17 @@ const MealItem = (props) => {
       {loadedMeals.map((meal) => {
         return (
           <li className="meal-item" key={meal.id}>
-          <img src={meal.image} alt={meal.name} />
-          <div>
-            <h3>{meal.name}</h3>
-            <p className="meal-item-price">{meal.price}</p>
-            <p className="meal-item-description">{meal.description}</p>
-          </div>
-          <p className="meal-item-action">
-            <button> Add To Cart </button>
-          </p>
-        </li>
-        )
+            <img src={meal.image} alt={meal.name} width={50} height={50} />
+            <div>
+              <h3>{meal.name}</h3>
+              <p className="meal-item-price">{meal.price}</p>
+              <p className="meal-item-description">{meal.description}</p>
+            </div>
+            <p className="meal-item-action">
+              <button> Add To Cart </button>
+            </p>
+          </li>
+        );
       })}
     </>
   );
